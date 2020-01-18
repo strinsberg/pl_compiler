00c8fcbf773eaf101e01ca03bc8bbd6ba5153d92
Steven Deutekom <deutekom@uleth.ca>
2020-01-18 12:41:00 -0700

    Filled in a little documentation.

--- include/Token.h
+++ include/Token.h
@@ -9,17 +9,41 @@ class Token {
   Token();
   Token(Symbol sym, std::string lexeme = "", int val = -1);
 
+  /**
+    * Returns the symbol.
+    */ 
   Symbol getSymbol();
+  
+  /**
+    * Returns the lexeme.
+    */
   std::string getLexeme();
-  int getVal();  // type attvalue
+  
+  /**
+    * Returns the value.
+    */
+  int getVal();
 
+  /**
+    * Sets the symbol.
+    */
   void setSymbol(Symbol sym);
+
+  /**
+    * Sets the lexeme.
+    */
+  void setLexeme(std::string);
+
+  /**
+    * Sets the value.
+    */
   void setVal(int val);
+
 
  private:
   Symbol sname;
   std::string lexeme;
-  int val;  // type attvalue
+  int val;
 
 };
 
