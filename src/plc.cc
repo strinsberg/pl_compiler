124ad5a64be544980f91d2c5d1210b672fe16e91
Steven Deutekom <deutekom@uleth.ca>
2020-01-25 14:08:06 -0700

    Setup main properly to take command line arguments

--- src/plc.cc
+++ src/plc.cc
@@ -4,26 +4,45 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-#include <getopt>
+#include <getopt.h>
 
 using namespace std;
 
-int main(int argc, char** argc) {
+int main(int argc, char** argv) {
   int opt;
 
-  while((opt = getopt(argc, argv, "")) != 1) {
+  std::string outfile;
+  while((opt = getopt(argc, argv, "o:")) != -1) {
     switch (opt) {
-      default:
+      case 'o':
+        outfile = std::string(optarg);
         break;
+      default:
+        cerr << "Invalid Argument: " << (char)opt << endl;
+        return 1;
     }
   }
 
+  if (optind >= argc) {
+    cerr << "No filename given" << endl;
+    return 2;
+  }
 
-  std::ifstream fs("test/scannerTest.pl");
+  std::string filename(argv[optind]);
+  std::ifstream fs(filename);
+
+  if (!fs.is_open()) {
+    cerr << "Invalid input file: " << filename << endl;
+    return 3;
+  }
+
+  if (outfile == "")
+    outfile = "pl.out";
+  std::ofstream ofs(outfile);
 
   SymbolTable sym;
   Scanner scanner(fs, sym);
-  Administration admin(std::cout, scanner);
+  Administration admin(ofs, scanner);
 
   admin.scan();
 
