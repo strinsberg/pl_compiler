474961dd3e08ccbaec77ef03f718b722a17de681
Ricky (Ricky) Bueckert <buer3200@36torch.cs.uleth.ca>
2020-01-25 11:01:23 -0700

    Error Printing

--- src/Administration.cc
+++ src/Administration.cc
@@ -1,11 +1,12 @@
 #include "Administration.h"
 #include "Scanner.h"
+#include "Symbol.h"
 #include <fstream>
 #include <iostream>
 
 
 Administration::Administration(std::ostream& ofs, Scanner& sc)
-    : fout(ofs), scanner(sc), lineNum(0), correctLine(true), errorCount(0)  {}
+    : fout(ofs), scanner(sc), lineNum(1), correctLine(true), errorCount(0)  {}
 
 
 void Administration::newLine() {
@@ -18,6 +19,17 @@ void Administration::error(std::string text) {
   std::cerr << "Error: Line: " << lineNum << " -> " << text << std::endl;
 }
 
+void Administration::checkError(Token ntoken){
+  if(ntoken.getSymbol() == Symbol::CHAR_ERR && correctLine == true) {
+    error("Invalid Char ( " + ntoken.getLexeme() + " )");
+    correctLine = false;
+    errorCount++;
+  } else if(ntoken.getSymbol() == Symbol::FULL_TAB && correctLine == true) {
+    error("Symbol Table is Full");
+    correctLine = false;
+    errorCount++;
+  }
+}
 
 int Administration::scan() {
   Token current;
@@ -25,6 +37,10 @@ int Administration::scan() {
 
   while (current.getSymbol() != Symbol::ENDFILE) {
     current = scanner.getToken();
+    checkError(current);
+    if(current.getSymbol() == Symbol::NEWLINE) {
+      newLine();
+    }
     current.toString(fout);
     fout << std::endl;
   }
