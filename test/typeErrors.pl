26d0e98fc7f4a83d6f457b2320e03192e6fa4f39
Steven Deutekom <deutekom@uleth.ca>
2020-03-11 14:21:32 -0600

    Worked on tests and updated technical documentation.

--- test/typeErrors.pl
+++ test/typeErrors.pl
@@ -3,11 +3,23 @@ begin
   integer array A[n];
   integer a, b, c;
   Boolean e, f, g;
+  float h;
+
+  proc set(var integer a)
+  begin
+    integer b;
+    a := b;
+  end;
+
+  call set(45);  $ Should be an error
+  call set(h);
 
   a, b, c := e, f, g;
   a := true | false;
   e := 2 + 4;
   b := 4 * 6.8;
+  h := a;
+  h := e;
 
   if (4 * 5) -> skip; fi;
   do (n + 3.6) -> skip; od;
